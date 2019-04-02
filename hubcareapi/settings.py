1f26dd8f84b8179424186d6c6216a0bd42054bff
Cleber Junior <cjj.castro3@gmail.com>
2019-04-01 21:46:58 -0300

    Created initial apps versions and add django rest app

--- hubcareapi/settings.py
+++ hubcareapi/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'commits',
+    'issues',
+    'pull_requests',
+    'community',
 ]
 
 MIDDLEWARE = [
---
+++ pull_requests/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PullRequestsConfig(AppConfig):
+    name = 'pull_requests'
