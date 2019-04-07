10a55e8faae7f07f0e4373303d0df62cf272e5cc
Jacoapolinario <jacoappolinario@hotmail.com>
2019-04-06 23:56:09 -0300

    Erased the tests and updated the views added license in the database, only when it does not exist in the database it from error 404 if the repository does not exist valid if the saved data is older than one day
    Signed-off-by: Jacoapolinario <jacoappolinario@hotmail.com>
    Co-authored-by: Brian <brianlui2387@gmail.com>
    Co-authored-by: Cleber <cjj.castro3@gmail.com>

--- community/views.py
+++ community/views.py
@@ -4,22 +4,35 @@ from rest_framework.response import Response
 from .serializers import LicenseSerializer
 from .models import License
 import requests
+from datetime import date
 
 # Create your views here.
 
 class LicenseView(APIView):
     
     def get(self, request, owner, repo):
-        print(repo)
-        url = 'https://api.github.com/repos/'
-        result = requests.get(url + owner + '/' + repo)
-        result = result.json()
-        try: 
-            if (result['license']) != None:
-                return Response(result['license'])
+        
+        all_license = License.objects.all().filter(owner=owner,repo=repo)
+        
+        license_serializer = LicenseSerializer(all_license, many=True)
+          
+        if (license_serializer.data == [] or license_serializer.data[0].date < date.today()):
+            
+            url = 'https://api.github.com/repos/'
+            result = requests.get(url + owner + '/' + repo)
+            github_data = result.json()
+            
+            if (result.status_code == 404):
+                raise Http404
+            elif (github_data['license']!= None):
+                License.objects.create(owner=owner,repo=repo,have_license=True,date=date.today())
+                
             else:
-                return Response(False)
-        except:
-            raise Http404
+                License.objects.create(owner=owner,repo=repo,have_license=False)
+        
+        license = License.objects.all().filter(owner=owner,repo=repo)
+        license_serialized = LicenseSerializer(license, many=True)   
+        print(license_serialized.data)
+        return Response(license_serialized.data[0])
 
-      
+        
