1da547b24cad8856f6a2122209ab8f436f318f3f
brian2397 <brianlui2387@gmail.com>
2019-04-08 12:30:47 -0300

    Add Tests Create the Def setUp, to use the requestfactory and create a object Create the Def test_details to test the view
    Signed-off-by: brian2397 <brianlui2387@gmail.com>
    Co-authored-by: jacoapolinario <jacoappolinario@hotmail.com>

--- community/tests.py
+++ community/tests.py
@@ -1,1 +1,19 @@
-from django.test import TestCase
+from django.test import RequestFactory, TestCase
+from .models import License
+from .views import LicenseView
+from datetime import date
+
+class LicenseTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.license = License.objects.create(
+            owner='cleber', repo='cremilda', have_license=True, date=date.today()
+        )
+    
+    def test_details(self):
+        request = self.factory.get('/community/license/cleber/cremilda')
+
+        request.license = self.license
+
+        response = LicenseView().get(request,owner='cleber',repo='cremilda')
+        self.assertEqual(response.status_code, 200)
