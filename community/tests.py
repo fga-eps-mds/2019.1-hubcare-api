1e28eca7676bd38e526229d50bd6e04a30b61a5c
Francisco Heronildo <heronildo.2010@hotmail.com>
2019-04-09 09:32:56 -0300

    Create test to community view
    Signed-off-by: Francisco Heronildo <heronildo.2010@hotmail.com>
    Co-authored-by: Cleber Castro <cjj.castro3@gmail.com>

--- community/tests.py
+++ community/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from .models import Community
+from .views import PullRequestTemplateView
 
-# Create your tests here.
+class TestCommunity(TestCase):
+
+    def setUp(self):
+        self.factory = RequestFactory()
+        
+    def test_community(self):
+        request = self.factory.get('/pull_request_template/fga-eps-mds/2019.1-hubcare-api')
+
+        response = PullRequestTemplateView.as_view()(request, 'fga-eps-mds', '2019.1-hucare-api')
+
+        self.assertEqual(response.status_code, 200)
+        
+
