a913bf078da28e79304feb0a567a86f669a75479
Cleber Junior <cjj.castro3@gmail.com>
2019-04-10 13:59:03 -0300

    Added mock to 404 test.
    Signed-off-by: Cleber Junior <cjj.castro3@gmail.com>
    Co-authored-by: brian2397 <brianlui2387@gmail.com>

--- community/tests/test_license.py
+++ community/tests/test_license.py
@@ -2,7 +2,25 @@ from django.test import RequestFactory, TestCase
 from community.models.license_model import License
 from community.views.license_view import LicenseView
 from datetime import date
+from unittest import mock
 
+
+# This method will be used by the mock to replace requests.get
+def mocked_requests_get(*args, **kwargs):
+    class MockResponse:
+        def __init__(self, json_data, status_code):
+            self.json_data = json_data
+            self.status_code = status_code
+
+        def json(self):
+            return self.json_data
+
+    # if args[0] == 'http://someurl.com/test.json':
+    #     return MockResponse({"key1": "value1"}, 200)
+    # elif args[0] == 'http://someotherurl.com/anothertest.json':
+    #     return MockResponse({"key2": "value2"}, 200)
+
+    return MockResponse(None, 404)
 
 class LicenseTest(TestCase):
     def setUp(self):
@@ -14,8 +32,9 @@ class LicenseTest(TestCase):
             date=date.today(),
         )
 
-    def test_RepositoryExistence(self):
-        request = self.factory.get('/community/license/cleber/cremilda')
+    @mock.patch('community.views.license_view.requests.get', side_effect=mocked_requests_get)
+    def test_RepositoryExistence(self, mock_get):
+        request = self.factory.get('/community/license/cleber/desenho')
         response = LicenseView.as_view()(request, 'cleber', 'desenho')
         self.assertEqual(response.status_code, 404)
 
