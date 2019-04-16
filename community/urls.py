4b579980d3696cf3b08581a572c2a231e247b828
Francisco Heronildo <heronildo.2010@hotmail.com>
2019-04-16 15:15:26 -0300

    Fixed pep8 style guide errors
    Signed-off-by: Francisco Heronildo <heronildo.2010@hotmail.com>
    Co-authored-by: Vitor Meireles Oliveira <vitorrr.meireles@hotmail.com>

--- community/urls.py
+++ community/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from community.views.issue_templates_views import IssueTemplatesView
 
+aux = 'issue_templates/<str:owner>/<str:repo>/'
 urlpatterns = [
-              path('issue_templates/<str:owner>/<str:repo>', IssueTemplatesView.as_view())
-              ]
+    path(aux, IssueTemplatesView.as_view())
+]
--- community/views/issue_templates_views.py
+++ community/views/issue_templates_views.py
@@ -1,7 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from community.models.issue_templates_model import IssueTemplates
-from community.serializers.issue_templates_serializer import IssueTemplatesSerializer
+from community.serializers.issue_templates_serializer \
+    import IssueTemplatesSerializer
 from datetime import datetime, timezone
 import requests
 
@@ -9,26 +10,32 @@ import requests
 class IssueTemplatesView(APIView):
 
     def get(self, request, owner, repo, format=None):
-        issue_templates = IssueTemplates.objects.all().filter(owner=owner, repo=repo)
+        issue_templates = IssueTemplates.objects.all().filter(
+            owner=owner,
+            repo=repo
+        )
 
-        if(not issue_templates): 
-            issue_serialized = IssueTemplatesSerializer(issue_templates, many=True)
-            url1 = 'https://api.github.com/repos/' 
+        if(not issue_templates):
+            issue_serialized = IssueTemplatesSerializer(
+                issue_templates,
+                many=True
+            )
+            url1 = 'https://api.github.com/repos/'
             url2 = '/contents/.github/ISSUE_TEMPLATE.md'
             result = url1 + owner + '/' + repo + url2
             github_request = requests.get(result)
-        
             if(github_request.status_code == 200):
                 IssueTemplates.objects.create(
-                    owner=owner, 
-                    repo=repo, 
-                    issue_templates=True, 
+                    owner=owner,
+                    repo=repo,
+                    issue_templates=True,
                     date=datetime.now(timezone.utc)
                 )
             else:
-                IssueTemplates.objects.create(owner=owner, 
-                    repo=repo, 
-                    issue_templates=False, 
+                IssueTemplates.objects.create(
+                    owner=owner,
+                    repo=repo,
+                    issue_templates=False,
                     date=datetime.now(timezone.utc)
                 )
         elif(check_date(issue_templates)):
@@ -52,9 +59,13 @@ class IssueTemplatesView(APIView):
                     date=datetime.now(timezone.utc)
                 )
 
-        issue_templates = IssueTemplates.objects.all().filter(owner=owner, repo=repo)
-        issue_serializer = IssueTemplatesSerializer(issue_templates, many=True)
+        issue_templates = IssueTemplates.objects.all().filter(
+            owner=owner,
+            repo=repo
+        )
+        issue_serialized = IssueTemplatesSerializer(issue_templates, many=True)
         return Response(issue_serialized.data[0])
+
 
 def check_date(issue_templates):
     datetime_now = datetime.now(timezone.utc)
