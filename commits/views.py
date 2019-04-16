da18c5260cb65f1afec9f76a238ee97a2b848167
Vitor Alves <vitor.alves07750@outlook.com>
2019-04-15 21:51:36 -0300

    Added new lists and modified the response parameter
    Changes were made to commits / views.py.
    CommitsLastThirtyDays, authorsCommits, and out lists have been added.
    also changed the return response which is now the list out
    
    Signed-off-by: Vitor Alves <vitor.alves07750@outlook.com>

--- commits/views.py
+++ commits/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from .models import DifferentsAuthors 
 from .serializers import DifferentsAuthorsSerializers
 import requests
+import datetime
 
 class DifferentsAuthorsView(APIView):
 
@@ -11,4 +12,23 @@ class DifferentsAuthorsView(APIView):
         differentsauthors = DifferentsAuthors.objects.all().filter(owner=owner,repo=repo)
         differentsauthors_serialized = DifferentsAuthorsSerializers(differentsauthors, many = True)
         github_request = requests.get('https://api.github.com/repos/' + owner + '/' + repo + '/commits')
-        return Response(github_request.json())
+        github_data = github_request.json()
+        present = datetime.datetime.today()
+        days = datetime.timedelta(days=30)
+        commitsLastThirtyDays = []
+        authorsCommits = []
+        out = []
+
+        for commit in github_data:
+            commit['commit']['committer']['date'].split('T')[0]
+            past = datetime.datetime.strptime(commit['commit']['committer']['date'], "%Y-%m-%dT%H:%M:%SZ")
+            commitsDay = present - days
+            if((past > commitsDay)):
+                commitsLastThirtyDays.append(commit['commit']['committer']['date'].split('T')[0])
+                print(past.date())
+                authorsCommits.append(commit['commit']['author']['name'])
+
+       
+        out.append(authorsCommits)
+        out.append(commitsLastThirtyDays)
+        return Response(out)
