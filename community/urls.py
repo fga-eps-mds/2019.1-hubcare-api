3dacc82fe3b069fb9c1c092dab0d346ca3c54fbf
Cleber Junior <cjj.castro3@gmail.com>
2019-04-16 11:04:29 -0300

    Fix url format

--- community/urls.py
+++ community/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from community.views.contribution_guide_views import ContributionGuideView
 
 urlpatterns = [
-              path('contribution_guide/<str:owner>/<str:repo/>', ContributionGuideView.as_view())
+              path('contribution_guide/<str:owner>/<str:repo>/', ContributionGuideView.as_view())
               ]
