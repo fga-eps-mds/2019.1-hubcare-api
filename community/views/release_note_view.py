96d77570f7381da69e1326332babea21be825599
Vitor Alves <vitor.alves07750@outlook.com>
2019-04-18 23:00:06 -0300

    The return was changed
    In the file release_note_view.py were added
    conditional methods to change the return
    that is now returning a boolean
    
    Signed-off-by: Vitor Alves <vitor.alves07750@outlook.com>
    Co-authored-by:  Jacó Apolinário <jacoappolinario@hotmail.com>

--- community/views/release_note_view.py
+++ community/views/release_note_view.py
@@ -4,24 +4,37 @@ from community.models.release_note_models \
     import ReleaseNoteCheck
 from community.serializers.release_note_serializers \
     import ReleaseNoteCheckSerializers
-from datetime import datetime
+from datetime import datetime,timedelta
 import requests
 
 
 class ReleaseNoteCheckView(APIView):
 
     def get(self, request, owner, repo):
-        realeasenotecheck = ReleaseNoteCheck.objects.all().filter(
+        releasenotecheck = ReleaseNoteCheck.objects.all().filter(
             owner=owner,
             repo=repo
         )
-        realeasenotecheck_serialized = ReleaseNoteCheckSerializers(
-            realeasenotecheck,
+        releasenotecheck_serialized = ReleaseNoteCheckSerializers(
+            releasenotecheck,
             many=True
         )
         github_request = requests.get(
             'https://api.github.com/repos/' + owner + '/' + repo + '/releases'
         )
         github_data = github_request.json()
+        present = datetime.today()
+        days = timedelta(days=90)
+        releaseDays = present - days
+        releaseLastNinetyDays = []
 
-        return Response(github_request.json())
+        if(github_data != []):
+            releaseDate = datetime.strptime(github_data[0]['created_at'],"%Y-%m-%dT%H:%M:%SZ")
+            if(releaseDate > releaseDays):
+                return Response(True)
+            else:
+                return Response(False)
+        else:
+            return Response(False)
+
+        
