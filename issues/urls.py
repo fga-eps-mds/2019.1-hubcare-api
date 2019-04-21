dc6b370d00bb8bd263a6baba3bd5e8262a28b4bd
Francisco Heronildo <heronildo.2010@hotmail.com>
2019-04-20 22:27:11 -0300

    Created to views following the measurement model for the Good First Issue Created Good First Issue URLs Created the label_good_first_issue, label_goodfirstissue and label_good_first_issue_spaces contatens to make api requisitions
    Signed-off-by: Francisco Heronildo <heronildo.2010@hotmail.com>
    Co-authored-by: Vitor Meireles <vitorrr.meireles@hotmail.com>

--- issues/urls.py
+++ issues/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from issues.views.help_wanted_views import HelpWantedView
+from issues.views.good_first_issue_views import GoodFirstIssueView
+
+url_gfi = 'good_first_issue/<str:owner>/<str:repo>/'
 
 urlpatterns = [
     path('help_wanted/<str:owner>/<str:repo>/', HelpWantedView.as_view()),
+    path(url_gfi, GoodFirstIssueView.as_view())
 ]
