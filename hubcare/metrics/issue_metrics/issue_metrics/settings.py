dd97806d69bde7673c6c0088e5a4e2625bf5140e
Hiroshi <lucas.hh@hotmail.com>
2019-04-22 17:57:01 -0300

    Add localhost to settings in apps and migration in gitignore

--- hubcare/metrics/issue_metrics/issue_metrics/settings.py
+++ hubcare/metrics/issue_metrics/issue_metrics/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'v=!au)blx1o)+k%&9nfi8qa^q!9yt_$n@c1o0wx9-(_f3k@y)-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'good_first_issue',
-    'help_wanted'
+    'help_wanted',
+    'activity_rate',
 ]
 
 MIDDLEWARE = [
