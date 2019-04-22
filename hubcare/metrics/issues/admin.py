189c6bdc74a7a6d29a36a70c1539c3e7f9bc016c
Rômulo Souza <romulovinicius10@gmail.com>
2019-04-21 23:35:28 -0300

    Removed old issue structure

--- hubcare/metrics/issues/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from issues.models.help_wanted_models import HelpWanted
-
-# Register your models here.
-admin.site.register(HelpWanted)
--- hubcare/metrics/issues/models/good_first_issue_model.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class GoodFirstIssue(models.Model):
-    owner = models.CharField(max_length=200)
-    repo = models.CharField(max_length=200)
-    total_issues = models.IntegerField(default=0)
-    good_first_issue = models.IntegerField(default=0)
-    date_time = models.DateTimeField(default=None)
