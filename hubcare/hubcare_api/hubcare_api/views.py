5fef24136de628b02d7dcd675aceb7c86b1e867c
brian2397 <brianlui2387@gmail.com>
2019-04-22 23:08:18 -0300

    Taking only the Boolean element of each request
    Signed-off-by: brian2397 <brianlui2387@gmail.com>
    Co-authored-by: Franciso Heronildo <heronildo.2010@hotmail.com>

--- hubcare/hubcare_api/hubcare_api/views.py
+++ hubcare/hubcare_api/hubcare_api/views.py
@@ -13,30 +13,39 @@ class SupportQuestion(APIView):
         if(github_request.status_code == 200):
             url = URL_COMMMUNITY + 'readme/' + owner + '/' + repo
             readme_metric = requests.get(url)
-            print(readme_metric.json())
-
+            print(readme_metric.json()[0]['readme'])
+            
+    
             url = URL_COMMMUNITY + 'issue_template/' + owner + '/' + repo
             issue_template_metric = requests.get(url)
-            print(issue_template_metric.json())
+            print(issue_template_metric.json()['issue_templates'])
+            
 
             url = URL_COMMMUNITY + 'license/' + owner + '/' + repo
             license_metric = requests.get(url)
-            print(license_metric.json())
+            print(license_metric.json()['have_license'])
+            
 
             url = URL_COMMMUNITY + 'description/' + owner + '/' + repo
             description_metric = requests.get(url)
-            print(description_metric.json())
+            print(description_metric.json()['description'])
+            
 
             url = URL_COMMMUNITY + 'code_of_conduct/' + owner + '/' + repo
             code_of_conduct_metric = requests.get(url)
-            print(code_of_conduct_metric.json())
+            print(code_of_conduct_metric.json()['code_of_conduct'])
+            
 
             url = URL_COMMMUNITY + 'release_note/' + owner + '/' + repo
             release_note_metric = requests.get(url)
-            print(release_note_metric.json())            
+            print(release_note_metric.json()['response'])
+            
+
+            
+
+        else:
+            raise Http404
 
         return Response("ok")
-
-
 
 
