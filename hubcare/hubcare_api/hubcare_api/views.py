2d8e775e9dbb362d8add0f06aa6d50271f0f6f14
brian2397 <brianlui2387@gmail.com>
2019-04-22 23:22:41 -0300

    Change the variable names
    Signed-off-by: brian2397 <brianlui2387@gmail.com>
    Co-authored-by: Francisco Heronildo <heronildo.2010@hotmail.com>

--- hubcare/hubcare_api/hubcare_api/views.py
+++ hubcare/hubcare_api/hubcare_api/views.py
@@ -13,54 +13,54 @@ class SupportQuestion(APIView):
         if(github_request.status_code == 200):
             url = URL_COMMMUNITY + 'readme/' + owner + '/' + repo
             readme_metric = requests.get(url)
-            print(readme_metric.json()[0]['readme'])
-            x = int(readme_metric.json()[0]['readme'])
+            readme_int = int(readme_metric.json()[0]['readme'])
     
             url = URL_COMMMUNITY + 'issue_template/' + owner + '/' + repo
             issue_template_metric = requests.get(url)
-            print(issue_template_metric.json()['issue_templates'])
-            y = int(issue_template_metric.json()['issue_templates'])
+            issue_template_int = int(issue_template_metric.json()['issue_templates'])
 
             url = URL_COMMMUNITY + 'license/' + owner + '/' + repo
             license_metric = requests.get(url)
-            print(license_metric.json()['have_license'])
-            z = int(license_metric.json()['have_license'])
+            license_int = int(license_metric.json()['have_license'])
 
             url = URL_COMMMUNITY + 'description/' + owner + '/' + repo
             description_metric = requests.get(url)
-            print(description_metric.json()['description'])
-            w = int(description_metric.json()['description'])
+            description_int = int(description_metric.json()['description'])
 
             url = URL_COMMMUNITY + 'code_of_conduct/' + owner + '/' + repo
             code_of_conduct_metric = requests.get(url)
-            print(code_of_conduct_metric.json()['code_of_conduct'])
-            u = int(code_of_conduct_metric.json()['code_of_conduct'])
+            code_of_conduct_int = int(code_of_conduct_metric.json()['code_of_conduct'])
 
             url = URL_COMMMUNITY + 'release_note/' + owner + '/' + repo
             release_note_metric = requests.get(url)
-            print(release_note_metric.json()['response'])
-            v = int(release_note_metric.json()['response'])
+            release_note_int = int(release_note_metric.json()['response'])
 
-            support_metric = calculate_support_metric(x, y, z, w, u, v)
+            support_metric = calculate_support_metric(readme_int,
+                                                    issue_template_int,
+                                                    license_int,
+                                                    description_int,
+                                                    code_of_conduct_int,
+                                                    release_note_int
+            )
 
         else:
             raise Http404
 
         return Response(support_metric)
 
-def calculate_support_metric(readme_metric,
-                            issue_template_metric,
-                            license_metric,
-                            description_metric,
-                            code_of_conduct_metric,
-                            release_note_metric
+def calculate_support_metric(readme_int,
+                            issue_template_int,
+                            license_int,
+                            description_int,
+                            code_of_conduct_int,
+                            release_note_int
                         ):
 
-    support_metric = (readme_metric*HEIGHT_README_SUPPORT
-                    +issue_template_metric*HEIGHT_ISSUE_TEMPLATE_SUPPORT
-                    +license_metric*HEIGHT_LICENSE_SUPPORT
-                    +description_metric*HEIGHT_DESCRIPTION_SUPPORT
-                    +code_of_conduct_metric*HEIGHT_CODE_OF_CONDUCT_SUPPORT
-                    +release_note_metric*HEIGHT_RELEASE_NOTE_SUPPORT)/16
+    support_metric = (readme_int*HEIGHT_README_SUPPORT
+                    +issue_template_int*HEIGHT_ISSUE_TEMPLATE_SUPPORT
+                    +license_int*HEIGHT_LICENSE_SUPPORT
+                    +description_int*HEIGHT_DESCRIPTION_SUPPORT
+                    +code_of_conduct_int*HEIGHT_CODE_OF_CONDUCT_SUPPORT
+                    +release_note_int*HEIGHT_RELEASE_NOTE_SUPPORT)/16
 
     return support_metric
