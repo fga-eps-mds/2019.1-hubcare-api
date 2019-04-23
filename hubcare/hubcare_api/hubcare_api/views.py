bd6828124d275166cc70335fa19d17280f9a17fb
brian2397 <brianlui2387@gmail.com>
2019-04-22 23:11:32 -0300

    Convert each Boolean to int
    Signed-off-by: brian2397 <brianlui2387@gmail.com>
    Co-authored-by: Francisco Heronildo <heronildo.2010@hotmail.com>

--- hubcare/hubcare_api/hubcare_api/views.py
+++ hubcare/hubcare_api/hubcare_api/views.py
@@ -14,32 +14,32 @@ class SupportQuestion(APIView):
             url = URL_COMMMUNITY + 'readme/' + owner + '/' + repo
             readme_metric = requests.get(url)
             print(readme_metric.json()[0]['readme'])
-            
+            x = int(readme_metric.json()[0]['readme'])
     
             url = URL_COMMMUNITY + 'issue_template/' + owner + '/' + repo
             issue_template_metric = requests.get(url)
             print(issue_template_metric.json()['issue_templates'])
-            
+            y = int(issue_template_metric.json()['issue_templates'])
 
             url = URL_COMMMUNITY + 'license/' + owner + '/' + repo
             license_metric = requests.get(url)
             print(license_metric.json()['have_license'])
-            
+            z = int(license_metric.json()['have_license'])
 
             url = URL_COMMMUNITY + 'description/' + owner + '/' + repo
             description_metric = requests.get(url)
             print(description_metric.json()['description'])
-            
+            w = int(description_metric.json()['description'])
 
             url = URL_COMMMUNITY + 'code_of_conduct/' + owner + '/' + repo
             code_of_conduct_metric = requests.get(url)
             print(code_of_conduct_metric.json()['code_of_conduct'])
-            
+            u = int(code_of_conduct_metric.json()['code_of_conduct'])
 
             url = URL_COMMMUNITY + 'release_note/' + owner + '/' + repo
             release_note_metric = requests.get(url)
             print(release_note_metric.json()['response'])
-            
+            v = int(release_note_metric.json()['response'])
 
             
 
