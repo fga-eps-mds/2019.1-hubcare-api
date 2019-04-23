efd84f1287e14e3c0b6d52f2587dfff7b4ca3463
brian2397 <brianlui2387@gmail.com>
2019-04-22 23:41:28 -0300

    Fix View Style and Constants style
    Signed-off-by: brian2397 <brianlui2387@gmail.com>
    Co-authored-by: Francisco Heronildo <heronildo.2010@hotmail.com>

--- hubcare/hubcare_api/hubcare_api/views.py
+++ hubcare/hubcare_api/hubcare_api/views.py
@@ -2,7 +2,7 @@ from django.http import Http404, HttpResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
 import requests
-from hubcare_api.constants import * 
+from hubcare_api.constants import *
 
 
 class SupportQuestion(APIView):
@@ -13,34 +13,41 @@ class SupportQuestion(APIView):
         if(github_request.status_code == 200):
             url = URL_COMMMUNITY + 'readme/' + owner + '/' + repo
             readme_metric = requests.get(url)
-            readme_int = int(readme_metric.json()[0]['readme'])
-    
+            readme_bool = readme_metric.json()[0]['readme']
+            readme_int = int(readme_bool)
+
             url = URL_COMMMUNITY + 'issue_template/' + owner + '/' + repo
-            issue_template_metric = requests.get(url)
-            issue_template_int = int(issue_template_metric.json()['issue_templates'])
+            issue_temp_metric = requests.get(url)
+            issue_temp_bool = issue_temp_metric.json()['issue_templates']
+            issue_temp_int = int(issue_temp_bool)
 
             url = URL_COMMMUNITY + 'license/' + owner + '/' + repo
             license_metric = requests.get(url)
-            license_int = int(license_metric.json()['have_license'])
+            license_bool = license_metric.json()['have_license']
+            license_int = int(license_bool)
 
             url = URL_COMMMUNITY + 'description/' + owner + '/' + repo
             description_metric = requests.get(url)
-            description_int = int(description_metric.json()['description'])
+            description_bool = description_metric.json()['description']
+            description_int = int(description_bool)
 
             url = URL_COMMMUNITY + 'code_of_conduct/' + owner + '/' + repo
-            code_of_conduct_metric = requests.get(url)
-            code_of_conduct_int = int(code_of_conduct_metric.json()['code_of_conduct'])
+            code_cond_metric = requests.get(url)
+            code_cond_bool = code_cond_metric.json()['code_of_conduct']
+            code_cond_int = int(code_cond_bool)
 
             url = URL_COMMMUNITY + 'release_note/' + owner + '/' + repo
             release_note_metric = requests.get(url)
-            release_note_int = int(release_note_metric.json()['response'])
+            release_note_bool = release_note_metric.json()['response']
+            release_note_int = int(release_note_bool)
 
-            support_metric = calculate_support_metric(readme_int,
-                                                    issue_template_int,
-                                                    license_int,
-                                                    description_int,
-                                                    code_of_conduct_int,
-                                                    release_note_int
+            support_metric = calculate_support_metric(
+                                readme_int,
+                                issue_temp_int,
+                                license_int,
+                                description_int,
+                                code_cond_int,
+                                release_note_int
             )
 
         else:
@@ -48,19 +55,22 @@ class SupportQuestion(APIView):
 
         return Response(support_metric)
 
-def calculate_support_metric(readme_int,
-                            issue_template_int,
-                            license_int,
-                            description_int,
-                            code_of_conduct_int,
-                            release_note_int
-                        ):
 
-    support_metric = (readme_int*HEIGHT_README_SUPPORT
-                    +issue_template_int*HEIGHT_ISSUE_TEMPLATE_SUPPORT
-                    +license_int*HEIGHT_LICENSE_SUPPORT
-                    +description_int*HEIGHT_DESCRIPTION_SUPPORT
-                    +code_of_conduct_int*HEIGHT_CODE_OF_CONDUCT_SUPPORT
-                    +release_note_int*HEIGHT_RELEASE_NOTE_SUPPORT)/16
+def calculate_support_metric(
+        readme_int,
+        issue_temp_int,
+        license_int,
+        description_int,
+        code_cond_int,
+        release_note_int
+):
+
+    support_metric = (
+        readme_int*HEIGHT_README_SUPPORT
+        + issue_temp_int*HEIGHT_ISSUE_TEMPLATE_SUPPORT
+        + license_int*HEIGHT_LICENSE_SUPPORT
+        + description_int*HEIGHT_DESCRIPTION_SUPPORT
+        + code_cond_int*HEIGHT_CODE_OF_CONDUCT_SUPPORT
+        + release_note_int*HEIGHT_RELEASE_NOTE_SUPPORT)/16
 
     return support_metric
