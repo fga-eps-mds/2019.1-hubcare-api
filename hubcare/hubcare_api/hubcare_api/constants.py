6328dc4d424896a71630b3dcf7f00519c8481c12
brian2397 <brianlui2387@gmail.com>
2019-04-25 14:01:50 -0300

    Request all the datas to answer the WelcomingQuestion
    Signed-off-by: brian2397 <brianlui2387@gmail.com>
    Co-authored-by: FranciscoHeronildo <heronildo.2010@hotmail.com>

--- hubcare/hubcare_api/hubcare_api/constants.py
+++ hubcare/hubcare_api/hubcare_api/constants.py
@@ -1,1 +1,3 @@
 URL_COMMIT = 'http://localhost:8001/'
+URL_COMMUNITY = 'http://localhost:8002/'
+URL_ISSUES = 'http://localhost:8003/'
--- hubcare/hubcare_api/hubcare_api/welcoming_views.py
+++ hubcare/hubcare_api/hubcare_api/welcoming_views.py
@@ -14,5 +14,40 @@ class WelcomingQuestion(APIView):
             url = URL_COMMIT + 'contributors/different_authors/' + owner + '/' + repo
             contributors_metric = requests.get(url)
             print(contributors_metric.json())
-        
+
+            url = URL_COMMUNITY + 'contribution_guide/' + owner + '/' + repo
+            contribution_guide_metric = requests.get(url)
+            print(contribution_guide_metric.json())
+
+            url = URL_ISSUES + 'help_wanted/' + owner + '/' + repo
+            help_wanted_metrics = requests.get(url)
+            print(help_wanted_metrics.json())
+
+            url = URL_ISSUES + 'good_first_issue/' + owner + '/' + repo
+            good_first_issue_metrics = requests.get(url)
+            print(good_first_issue_metrics.json())
+
+            url = URL_COMMUNITY + 'pull_request_template/' + owner + '/' + repo
+            pull_request_template_metrics = requests.get(url)
+            print(pull_request_template_metrics.json())
+
+            url = URL_COMMUNITY + 'description/' + owner + '/' + repo
+            description_metrics = requests.get(url)
+            print(description_metrics.json())
+
+            url = URL_COMMUNITY + 'code_of_conduct/' + owner + '/' + repo
+            code_of_conduct_metrics = requests.get(url)
+            print(code_of_conduct_metrics.json())
+
+            url = URL_COMMUNITY + 'readme/' + owner + '/' + repo
+            readme_metrics = requests.get(url)
+            print(readme_metrics.json())
+
+            url = URL_COMMUNITY + 'issue_template/' + owner + '/' + repo
+            issue_template_metrics = requests.get(url)
+            print(issue_template_metrics.json())
+
+            url = URL_COMMUNITY + 'license/' + owner + '/' + repo
+            license_metrics = requests.get(url)
+            print(license_metrics.json())
         return Response('ok')
