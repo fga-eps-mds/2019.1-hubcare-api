bc0aeaa25503aa4b578c9055ccdfef9d0e34fd9a
Vitor Alves <vitor.alves07750@outlook.com>
2019-05-04 23:03:41 -0300

    Added some functions to test file that check the existence of readme
    Signed-off-by: Vitor Alves <vitor.alves07750@outlook.com>
    Co-authored-by: Meireles <vitorrr.meireles@hotmail.com>
    Co-authored-by: Francisco Heronildo <heronildo.2010@hotmail.com>

--- hubcare/metrics/community_metrics/readme/tests.py
+++ hubcare/metrics/community_metrics/readme/tests.py
@@ -1,3 +1,43 @@
-from django.test import TestCase
+from django.test import RequestFactory, TestCase
+from readme.models import Readme
+from readme.views import ReadmeView
+from unittest import mock
+from datetime import date
 
-# Create your tests here.
+def mocked_requests_get(*args, **kwargs):
+
+    class MockResponse:
+        def __init__(self,json_data,status_code):
+            self.json_data =  json_data
+            self.status_code = status_code
+
+        def json(self):
+
+            return self.json_data
+        
+    if args[0] == 'https://api.github.com/repos/fga-eps-mds/2019.1-hubcare-api/contents/README.md':
+        return MockResponse({"readme":"value1"}, 200)
+    
+class ReadmeViewTest(TestCase):
+
+    def setUp(self):
+
+        self.factory = RequestFactory()
+        self.readme = Readme.objects.create(
+            owner='vitor',
+            repo ='treinamento_git',
+            readme= True,
+            date= date.today()
+        )
+    @mock.patch('readme.views.requests.get',
+                side_effect=mocked_requests_get)
+    def test_readme_existence(self,mock_get):
+
+        request = self.factory.get('readme/vitor/treinamento_git')
+        response = ReadmeView.as_view()(request,'vitor','treinamento_git')
+        self.assertEqual(response.status_code,200)
+        self.assertEqual(response.data['owner'],slice('vitor'))
+        self.assertEqual(response.data['repo'],'treinamento_git')
+        self.assertEqual(response.data['readme'],True)
+
+
