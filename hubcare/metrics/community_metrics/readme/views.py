70de01f226909ccb673ffe207753cce78e6a298e
Vitor Alves <vitor.alves07750@outlook.com>
2019-05-08 15:09:54 -0300

    Added new conditionals and function
    New conditionals were added in readme views to validate existence in the database and validate date. And a function was also made to validate that date.
    
    Signed-off-by: Vitor Alves <vitor.alves07750@outlook.com>
    Co-authored-by: Francisco Heronildo <heronildo.2010@hotmail.com>

--- hubcare/metrics/community_metrics/readme/views.py
+++ hubcare/metrics/community_metrics/readme/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from readme.serializers import ReadmeSerializer
 from readme.models import Readme
 import requests
-from datetime import date
+from datetime import date, datetime, timezone
 import os
 
 
@@ -12,12 +12,11 @@ class ReadmeView(APIView):
     def get(self, request, owner, repo):
 
         readme = Readme.objects.all().filter(owner=owner, repo=repo)
-        serialized = ReadmeSerializer(readme, many=True)
 
         username = os.environ['NAME']
         token = os.environ['TOKEN']
 
-        if (serialized.data == []):
+        if (not readme):
             url = 'https://api.github.com/repos/'
             url2 = '/contents/README.md'
             github_request = requests.get(url + owner + '/' + repo + url2,
@@ -37,7 +36,43 @@ class ReadmeView(APIView):
                     readme=False,
                     date=date.today()
                 )
+        elif(check_date(readme)):
+            url1 = 'https://api.github.com/repos/'
+            url2 = '/contents/README.md'
+            result = url1 + owner + '/' + repo + url2
+            github_request = requests.get(result, auth=(username,
+                                                        token))
 
-        readme = Readme.objects.all().filter(owner=owner, repo=repo)
-        serialized = ReadmeSerializer(readme, many=True)
-        return Response(serialized.data)
+            if(github_request.status_code == 200):
+                Readme.objects.filter().update(
+                    owner=owner,
+                    repo=repo,
+                    readme=True,
+                    date=datetime.now(timezone.utc)
+                )
+            else:
+                Readme.objects.filter().update(
+                    owner=owner,
+                    repo=repo,
+                    readme=False,
+                    date=datetime.now(timezone.utc)
+                )
+
+        readme = Readme.objects.all().filter(
+            owner=owner, repo=repo
+        )
+        serialized = ReadmeSerializer(
+            readme, many=True
+        )
+        return Response(serialized.data[0])
+
+
+def check_date(readme_check):
+    '''
+    verifies if the time difference between the last update and now is
+    greater than 24 hours
+    '''
+    datetime_now = date.today()
+    if(readme_check and (datetime_now - readme_check[0].date).days >= 1):
+        return True
+    return False
