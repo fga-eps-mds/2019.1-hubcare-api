95d83afda9d59cfe12c7ef492067852674742649
Vitor Meireles Oliveira <vitorrr.meireles@hotmail.com>
2019-05-09 14:45:33 -0300

    Testing 404 error and created an commit_object and commit_week_object; The first test is now working.
    Signed-off-by: Vitor Meireles Oliveira <vitorrr.meireles@hotmail.com>
    Co-authored-by: Brian the DevOps' king <brianlui2387@gmail.com>
    Co-authored-by: Romulo Souza <romulovinicius10@gmail.com>

--- hubcare/metrics/commit_metrics/commit_week/tests.py
+++ hubcare/metrics/commit_metrics/commit_week/tests.py
@@ -1,10 +1,39 @@
 from django.test import RequestFactory, TestCase
 from unittest import mock
 from datetime import datetime
+from commit_metrics.models import Commit
 from commit_week.models import CommitWeek
 from commit_week.views import CommitMonthView
 
 def mocked_requests_get(*args, **kwargs):
+    '''
+    This method will be used by the mock to replace requests.get
+    '''
+    class MockResponse:
+        '''
+        define response to mock request
+        '''
+        def __init__(self, json_data, status_code):
+            self.json_data = json_data
+            self.status_code = status_code
+
+        def json(self):
+            '''
+            return all datas in object
+            '''
+            return self.json_data
+
+    if args[0] == 'https://api.github.com/repos/test/repo_test':
+        return MockResponse({"commit_week": "value1"}, 200)
+    elif args[0] == 'https://api.github.com/repos/test/no_commit_week':
+        return MockResponse({"commit_week": None}, 200)
+    elif args[0] == 'https://api.github.com/repos/test/old_commit_week':
+        return MockResponse({"commit_week": "value1"}, 200)
+    elif args[0] == 'https://api.github.com/repos/test/old_commit_week':
+        return MockResponse({"commit_week": None}, 200)
+
+    return MockResponse(None, 404)
+
 
 
 
@@ -18,26 +47,16 @@ class CommitMonthViewTest(TestCase):
             setup test configs
         '''
         self.factory = RequestFactory()
+        commit_metric = self.commit_metric = Commit.objects.create(
+            owner='vitor',
+            repo='pricom',
+            date='2019-03-19'
+       )
         self.commit_week = CommitWeek.objects.create(
-            owner='brian',
-            repo='eda2',
-            date=datetime.now()
-        )
-        self.commit_week2 = CommitWeek.objects.create(
-            owner='brian',
-            repo='eda2',
-            date=datetime.now()
-        )
-        self.commit_week3 = CommitWeek.objects.create(
-            owner='brian',
-            repo='eda2',
-            date=datetime.now()
-        )
-        self.commit_week4 = CommitWeek.objects.create(
-            owner='brian',
-            repo='eda2',
-            date=datetime.now()
-        )
+            week=1,
+            quantity=5,
+            commit=commit_metric
+       )
 
     @mock.patch('commit_week.views.requests.get',
                 side_effect=mocked_requests_get)
@@ -46,4 +65,6 @@ class CommitMonthViewTest(TestCase):
         '''
             test if a repository exists
         '''
-        request = self.factory.get('')
+        request = self.factory.get('commit_week/cleber/desenho')
+        response = CommitMonthView.as_view()(request, 'cleber', 'desenho')
+        self.assertEqual(response.status_code, 404)
