22f3f1b3788a40e021d19731b5f6768e76cb6f6b
Vitor Meireles Oliveira <vitorrr.meireles@hotmail.com>
2019-05-12 10:37:33 -0300

    Added some functionalities to `tests.py` file of contributors
    Signed-off-by: Vitor Meireles Oliveira <vitorrr.meireles@hotmail.com>
    Co-authored-by: Brian <brianlui2387@gmail.com>

--- hubcare/metrics/commit_metrics/contributors/tests.py
+++ hubcare/metrics/commit_metrics/contributors/tests.py
@@ -6,7 +6,28 @@ from unittest import mock
 
 
 def mocked_requests_get(*args, **kwargs):
-    pass
+    class MockResponse:
+        def __init__ (self, json_data, status_code):
+            self.json_data = json_data
+            self.status_code = status_code
+
+        def json(self):
+            return self.json_data
+
+    return MockResponse(None, 404)
 
 class DifferentsAuthorsViewTest(TestCase):
-    pass
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.differentsauthors = DifferentsAuthors.objects.create(
+            owner='cleber',
+            repo='cremilda'
+        )
+
+    @mock.patch('differentauthors.views.requests_get',
+                side_effect=mocked_requests_get)
+    def test_repository_not_existence(self, mock_get):
+        pass
+    
+    def test_exists_in_db(self):
+        pass
