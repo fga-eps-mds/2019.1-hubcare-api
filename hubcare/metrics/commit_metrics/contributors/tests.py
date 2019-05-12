008f5e859cb8c2cfd68f32c713d2c632fb7b9431
Vitor Meireles Oliveira <vitorrr.meireles@hotmail.com>
2019-05-12 06:44:11 -0300

    Created an function and a class at `tests.py` file for contributors
    Signed-off-by: Vitor Meireles Oliveira <vitorrr.meireles@hotmail.com>
    Co-authored-by: Brian <brianlui2387@gmail.com>

--- hubcare/metrics/commit_metrics/contributors/tests.py
+++ hubcare/metrics/commit_metrics/contributors/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from contributors.models import DifferentsAuthors
+from contributors.views import DifferentsAuthorsView
+from datetime import datetime, timezone, date
+from unittest import mock
 
-# Create your tests here.
+
+def mocked_requests_get(*args, **kwargs):
+    pass
+
+class DifferentsAuthorsViewTest(TestCase):
+    pass
