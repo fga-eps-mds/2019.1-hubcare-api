2af87b0ac0b322edc92e5196f6cbbb3c327f2cce
Francisco Heronildo <heronildo.2010@hotmail.com>
2019-05-12 10:54:16 -0300

    Created Testing for Readme of the Community App
    It has been created tests for existence verification in the database, github api and validation of date of readme of the community app.
    
    Signed-off-by: Francisco Heronildo <heronildo.2010@hotmail.com>
    Co-authored-by: Vitor Alves <vitor.alves07750@outlook.com>

--- hubcare/metrics/community_metrics/readme/tests.py
+++ hubcare/metrics/community_metrics/readme/tests.py
@@ -1,8 +1,8 @@
 from django.test import RequestFactory, TestCase
 from readme.models import Readme
 from readme.views import ReadmeView
+from datetime import datetime, timezone
 from unittest import mock
-from datetime import date
 
 
 def mocked_requests_get(*args, **kwargs):
@@ -24,8 +24,11 @@ def mocked_requests_get(*args, **kwargs):
             return self.json_data
 
     url1 = 'https://api.github.com/repos/fga-eps-mds/2019.1-hubcare-api'
-    contents = '/contents/README.md'
-    if args[0] == url1 + contents:
+    url2 = 'https://api.github.com/repos/owner_date/repo_date'
+    url_content = '/contents/README.md'
+    if args[0] == url1 + url_content:
+        return MockResponse({"README": "name"}, 200)
+    elif args[0] == url2 + url_content:
         return MockResponse({"README": "name"}, 200)
     return MockResponse(None, 404)
 
@@ -36,11 +39,23 @@ class ReadmeViewTest(TestCase):
         Define Readme objects to tests
         '''
         self.factory = RequestFactory()
-        self.readme = Readme.objects.create(
+        Readme.objects.create(
             owner='owner_test',
             repo='repo_test',
             readme=True,
-            date=date.today()
+            date_time=datetime.now(timezone.utc)
+        )
+        Readme.objects.create(
+            owner='owner_date',
+            repo='repo_date',
+            readme=True,
+            date_time=datetime(2018, 5, 8, 15, 30, 45, 78910)
+        )
+        Readme.objects.create(
+            owner='owner_date2',
+            repo='repo_date2',
+            readme=True,
+            date_time=datetime(2018, 5, 8, 15, 30, 45, 78910)
         )
 
     def test_readme_existence_in_db(self):
@@ -91,3 +106,49 @@ class ReadmeViewTest(TestCase):
         self.assertEqual(response.data['owner'], 'not_exists')
         self.assertEqual(response.data['repo'], 'not_exists')
         self.assertEqual(response.data['readme'], False)
+
+    @mock.patch('readme.views.requests.get',
+                side_effect=mocked_requests_get)
+    def test_date_readme_existence(self, mock_get):
+        '''
+        test old date of readme in database
+        '''
+        url = 'readme/owner_date/repo_date'
+        request = self.factory.get(url)
+        response = ReadmeView.as_view()(
+            request,
+            'owner_date',
+            'repo_date'
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['owner'], 'owner_date')
+        self.assertEqual(response.data['repo'], 'repo_date')
+        self.assertEqual(response.data['readme'], True)
+        response_date = response.data['date_time']
+        date_strp = datetime.strptime(response_date[0:10], "%Y-%m-%d").date()
+        self.assertEqual(str(date_strp),
+                         str(datetime.now(timezone.utc).date()))
+
+    @mock.patch('readme.views.requests.get',
+                side_effect=mocked_requests_get)
+    def test_date_readme_not_existence(self, mock_get):
+        '''
+        test old date of readme in database
+        and the readme not exists
+        '''
+        url = 'readme/owner_date2/repo_date2'
+        request = self.factory.get(url)
+        response = ReadmeView.as_view()(
+            request,
+            'owner_date2',
+            'repo_date2'
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['owner'], 'owner_date2')
+        self.assertEqual(response.data['repo'], 'repo_date2')
+        self.assertEqual(response.data['readme'], False)
+        response_date = response.data['date_time']
+        date_strp = datetime.strptime(response_date[0:10], "%Y-%m-%d").date()
+        self.assertEqual(str(date_strp),
+                         str(datetime.now(timezone.utc).date()))
