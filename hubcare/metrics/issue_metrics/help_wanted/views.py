73cb2838f595f52e24dfd4a4f1175decf6249e24
Vitor Alves <vitor.alves07750@outlook.com>
2019-05-13 01:51:24 -0300

    Created functions in the Issue App for the Help Wanted
    help_wanted functions have been created for the Issue App, for the functions.py file with date validation functions, for calculating metrics, and for counting issues with the label help_wanted
    
    Signed-off-by: Vitor Alves <vitor.alves07750@outlook.com>
    Co-auhtored-by: Francisco Heronildo <heronildo.2010@hotmail.com>

--- hubcare/metrics/issue_metrics/help_wanted/views.py
+++ hubcare/metrics/issue_metrics/help_wanted/views.py
@@ -4,6 +4,8 @@ from help_wanted.models import HelpWanted
 from help_wanted.serializers import HelpWantedSerializer
 from datetime import datetime, timezone
 from issue_metrics import constants
+from issue_metrics.functions \
+    import check_datetime, get_metric_help_wanted, count_all_label
 import requests
 import json
 import os
@@ -34,7 +36,7 @@ class HelpWantedView(APIView):
                 date_time=datetime.now(timezone.utc)
             )
 
-        return Response(self.get_metric(owner, repo))
+        return Response(get_metric_help_wanted(HelpWanted, owner, repo))
 
     def get_total_helpwanted(self, url):
         '''
@@ -57,13 +59,13 @@ class HelpWantedView(APIView):
         checks possibilities for different aliases of help wanted
         '''
         if result:
-            help_wanted_issues = self.count_all_helpwanted(label_url, result)
+            help_wanted_issues = count_all_label(label_url, result)
         else:
             label_url = url + constants.label_helpwanted
             result = requests.get(label_url + page,
                                   auth=(username, token)).json()
             if result:
-                help_wanted_issues = self.count_all_helpwanted(
+                help_wanted_issues = count_all_label(
                     label_url,
                     result
                 )
@@ -72,52 +74,8 @@ class HelpWantedView(APIView):
                 result = requests.get(label_url + page,
                                       auth=(username, token)).json()
                 if result:
-                    help_wanted_issues = self.count_all_helpwanted(
+                    help_wanted_issues = count_all_label(
                         label_url,
                         result
                     )
         return total_issues, help_wanted_issues
-
-    def count_all_helpwanted(self, url, result):
-        '''
-        returns the number of help wanted issues in all pages
-        '''
-        username = os.environ['NAME']
-        token = os.environ['TOKEN']
-
-        count = 1
-        page = '&page='
-        help_wanted_issues = 0
-        while result:
-            count += 1
-            help_wanted_issues += len(result)
-            result = requests.get(url + page + str(count),
-                                  auth=(username, token)).json()
-        return help_wanted_issues
-
-    def get_metric(self, owner, repo):
-        '''
-        returns the metric of the repository
-        '''
-        help_wanted = HelpWanted.objects.all().filter(
-            owner=owner,
-            repo=repo
-        )[0]
-        if help_wanted.total_issues != 0:
-            rate = help_wanted.help_wanted_issues / help_wanted.total_issues
-        else:
-            rate = 0.0
-        rate = '{"rate":\"' + str(rate) + '"}'
-        rate_json = json.loads(rate)
-        return rate_json
-
-
-def check_datetime(help_wanted):
-    '''
-    verifies if the time difference between the last update and now is
-    greater than 24 hours
-    '''
-    datetime_now = datetime.now(timezone.utc)
-    if((datetime_now - help_wanted.date_time).days >= 1):
-        return True
-    return False
