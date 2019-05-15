a3defa3431d80efaf45f434a805a53dbea0ee930
Vitor Meireles Oliveira <vitorrr.meireles@hotmail.com>
2019-05-15 16:27:41 -0300

    Add part of test.py for pull_request_quality.
    Signed-off-by: Vitor Meireles Oliveira <vitorrr.meireles@hotmail.com>
    Co-authored-by: Vitor que na verdade é Jão <vitor.alves07750@outlook.com>
    Co-authored-by: Brian Luis I. L. da Silva <brianlui2387@gmail.com>
    Co-authored-by: Franchesco Telles <heronildo.2010@hotmail.com>

--- hubcare/metrics/pull_request_metrics/acceptance_quality/tests.py
+++ hubcare/metrics/pull_request_metrics/acceptance_quality/tests.py
@@ -1,3 +1,52 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from acceptance_quality.models import PullRequestQuality
+from acceptance_quality.views import PullRequestQualityView
+from datetime import datetime, timezone
+from unittest import mock
 
-# Create your tests here.
+
+def mocked_requests_get(*args, **kwargs):
+    '''
+        This method will be used by the mock to replace requests.get
+    '''
+    class MockResponse:
+        '''
+            Define response to mock request
+        '''
+
+
+class PullRequestQualityViewTest(TestCase):
+    '''
+        Test all methods to view class
+    '''
+    def setUp(self):
+        '''
+            Setup test configs
+        '''
+        self.factory = RequestFactory()
+        self.pullrequestquality = PullRequestQuality.objects.create()
+
+    @mock.patch('pullrequestquality.views.requests.get',
+                side_effect=mocked_requests_get)
+    def test_pull_request_quality_existence(self, mock_get):
+        '''
+            Test if the pull request quality already exists
+        '''
+
+    def test_pull_request_quality_existence_in_db(self):
+        '''
+            Test if there is pull request quality at db
+        '''
+
+    @mock.patch('license.views.request.get',
+                side_effect=mocked_requests_get)
+    def test_check_datetime_out(self, mock_get):
+        '''
+            Test if already has a score based on PRs
+        '''
+    @mock.patch('license.views.request.get',
+                side_effect=mocked_requests_get)
+    def test_check_datetime(self, mock_get):
+        '''
+            Test if the score it that old
+        '''
